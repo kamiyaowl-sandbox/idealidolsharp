268f84bcce61ed6ece4e65f85b15f641151773cb
Fumiya Katoh <psoc5lp@gmail.com>
2017-08-17 00:54:31 +0900

    隣接ノートの検出

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -59,9 +59,8 @@ namespace IdealIdolSharp.ViewModel {
                             //read
                             cap.Read(inputMat);
                             if (inputMat.Empty()) break;
-                            //update
+                            //差分画像から前回と今回のノート位置を推定
                             Cv2.Absdiff(inputMat, prevMat, diffMat);
-
                             var diffGrayMat = diffMat.CvtColor(OpenCvSharp.ColorConversion.BgrToGray);
                             var diffBinMat = diffGrayMat.Threshold(ProcessParam.Value.DiffGrayThreash, 255, OpenCvSharp.ThresholdType.Binary);
                             var circles = Cv2.HoughCircles(diffBinMat, OpenCvSharp.HoughCirclesMethod.Gradient,
@@ -71,16 +70,47 @@ namespace IdealIdolSharp.ViewModel {
                                 ProcessParam.Value.HoughCircleParam2,
                                 ProcessParam.Value.HoughCircleMinRadius,
                                 ProcessParam.Value.HoughCircleMaxRadius);
-
                             inputMat.CopyTo(prevMat);
-                            //draw
+
+                            //元画像を微分して、ホールドノートの推定をする（真縦のホールドは差分画像では追従不可)
+                            var inputGrayMat = inputMat.CvtColor(ColorConversion.BgrToGray);
+                            var inputSobelMat = inputGrayMat.Sobel(MatType.CV_8UC1,
+                               ProcessParam.Value.SobelXOrder,
+                               ProcessParam.Value.SobelYOrder);
+
+                            //円の図示
+                            //foreach (var c in circles) {
+                            //    inputMat.Circle((int)c.Center.X, (int)c.Center.Y, (int)c.Radius, CvColor.Red, 3);
+                            //}
+                            //同じノートの推論
+                            var circlesList = circles.ToList();
                             foreach (var c in circles) {
+                                circlesList.Remove(c);//これから検索するので消す
+                                var nearest = circlesList.Select(p => {
+                                    var d = p.Center - c.Center;
+                                    return new {
+                                        Point = p,
+                                        Distance = Math.Abs(d.X) + Math.Abs(d.Y)
+                                    };
+                                })
+                                .Where(x => x.Distance < ProcessParam.Value.NearCircleDistance)
+                                .OrderBy(p => p.Distance)
+                                .FirstOrDefault();
+
+                                if (nearest == null) continue;
+                                circlesList.Remove(nearest.Point);//検索済なので消す
+
                                 inputMat.Circle((int)c.Center.X, (int)c.Center.Y, (int)c.Radius, CvColor.Red, 3);
+                                inputMat.Circle((int)nearest.Point.Center.X, (int)nearest.Point.Center.Y, (int)nearest.Point.Radius, CvColor.Blue, 3);
+                                inputMat.Line((int)c.Center.X, (int)c.Center.Y, (int)nearest.Point.Center.X, (int)nearest.Point.Center.Y, CvColor.Purple, 3);
                             }
+
+
                             //show
                             Cv2.ImShow("input", inputMat);
                             Cv2.ImShow("diff", diffMat);
                             Cv2.ImShow("diffBin", diffBinMat);
+                            Cv2.ImShow("sobel", inputSobelMat);
                             if (Cv2.WaitKey(1) == 27) break;//ESC
                             CurrentFrame.Value = i;
                         }
@@ -96,8 +126,10 @@ namespace IdealIdolSharp.ViewModel {
         public double HoughCircleMinDist { get; set; } = 30;
         public double HoughCircleParam1 { get; set; } = 160;
         public double HoughCircleParam2 { get; set; } = 50;
-        public int HoughCircleMinRadius { get; set; } = 20;
+        public int HoughCircleMinRadius { get; set; } = 10;
         public int HoughCircleMaxRadius { get; set; } = 40;
-
+        public int SobelXOrder { get; internal set; } = 1;
+        public int SobelYOrder { get; internal set; } = 0;
+        public float NearCircleDistance { get; set; } = 100;
     }
 }
